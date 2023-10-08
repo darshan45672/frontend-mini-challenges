a1300292d5e707b9ae279c197139fc22ae1c9ca5
darshan45672 <drshnbhandary@gmail.com>
2023-10-08 12:44:04 +0530

    optimized code index.js

--- native/machine-coding/todo-list/index.js
+++ native/machine-coding/todo-list/index.js
@@ -3,55 +3,43 @@ const initialItems = ['JavaScript', 'HTML', 'CSS', 'React', 'Angular', 'Zustand'
 const listContainer = document.getElementById('listContainer');
 const todoItemInput = document.getElementById('todoItemInput');
 const todoForm = document.getElementById('todoForm');
-const todo = document.getElementById('todo');
+const todoTemplate = document.getElementById('todo').content;
 
-// Function to create a new todo item element
-function createTodoElement(todoItem) {
-  const todoElement = todo.content.cloneNode(true);
+function addTodoItem(todoItem) {
+  const todoElement = document.importNode(todoTemplate, true);
   const textElement = todoElement.querySelector('.text');
   const deleteButton = todoElement.querySelector('.delete');
   const editButton = todoElement.querySelector('.edit');
   const saveButton = todoElement.querySelector('.save');
-  
   textElement.textContent = todoItem;
-  
+
   deleteButton.addEventListener('click', () => {
     listContainer.removeChild(todoElement);
   });
-  
+
   editButton.addEventListener('click', () => {
-    const inputElement = document.createElement('input');
-    inputElement.value = textElement.textContent;
-    todoElement.replaceChild(inputElement, textElement);
-    editButton.textContent = '💾';
-    editButton.className = 'save';
+    textElement.contentEditable = true;
+    textElement.focus();
+    editButton.style.display = 'none';
+    saveButton.style.display = 'inline';
   });
-  
+
   saveButton.addEventListener('click', () => {
-    const textElement = document.createElement('span');
-    textElement.className = 'text';
-    textElement.textContent = inputElement.value;
-    todoElement.replaceChild(textElement, inputElement);
-    saveButton.textContent = '✏️';
-    saveButton.className = 'edit';
+    textElement.contentEditable = false;
+    editButton.style.display = 'inline';
+    saveButton.style.display = 'none';
   });
-  
-  return todoElement;
+
+  listContainer.appendChild(todoElement);
+  todoItemInput.value = '';
 }
 
-// Event listener for form submission
-todoForm.addEventListener('submit', e => {
+todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const todoItem = todoItemInput.value.trim();
   if (todoItem) {
-    const newTodoElement = createTodoElement(todoItem);
-    listContainer.appendChild(newTodoElement);
-    todoItemInput.value = '';
+    addTodoItem(todoItem);
   }
 });
 
-// Initialize the to-do list with initial items
-initialItems.forEach(todoItem => {
-  const newTodoElement = createTodoElement(todoItem);
-  listContainer.appendChild(newTodoElement);
-});
+initialItems.forEach(addTodoItem);
